0f49add26e0ba64c36913655c2e1abbd3198f855
Maxime Ambry <maxime.ambry@gmail.com>
2018-02-15 14:31:40 +0100

    Adding routes

--- serveur.js
+++ serveur.js
@@ -1,15 +1,30 @@
 var express = require('express');
 var app = express();
+var router = express.Router();
 var path = require('path');
 
 app.get('/', function(req, res) {
 	res.setHeader('Content-Type', 'text/html');
 	res.sendFile(path.join(__dirname + '/index.html'));
-})
-.use('/style', express.static(path.join(__dirname + '/style')))
-.use(function(req, res, next){
-    res.setHeader('Content-Type', 'text/plain');
-    res.status(404).send('Page introuvable !');
 });
+
+app.use('/style', express.static(path.join(__dirname + '/style')));
+
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+
+router.get("/about",function(req,res){
+  res.sendFile(__dirname + '/views/creation.html');
+});
+
+app.use("/",router);
+
+app.use("*",function(req,res){
+  res.sendFile(__dirname + "/public/404.html");
+});
+
+app.use("/api",router);
 
 app.listen(8080);
