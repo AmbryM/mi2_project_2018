d34ca4497f8a3c8b21f51e91a6d31550b63146a8
ValentinAndre <val.andre@hotmail.fr>
2018-02-15 14:00:10 +0100

    Ajout de la ligne créant la base de données

--- data/createBD.sql
+++ data/createBD.sql
@@ -21,7 +21,7 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `projet_web_dynamique`
 --
-
+CREATE DATABASE projet_web_dynamique;
 -- --------------------------------------------------------
 
 --
